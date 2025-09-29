93556ef8109fc124d2f38651dc1d541eb529e650
ansh-dubey7 <anshdubey32432@gmail.com>
2025-09-29 12:12:34 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5115,
+    allowedHosts: ['test.ujjainmahakal.com', 'http://localhost:5000'],
+  },
 })
